1e8f39e870c96214cc6a477e5b5179fabbc0d88f
Alexandru Iachimov <Alexandru.Iachimov@endava.com>
2017-06-13 11:47:57 +0300

    Added fonts, created POJO models, added JSON sample to assets folder

---
+++ AndroidThingsClient/app/src/main/java/com/example/androidthingsclient/models/Humidity.kt
@@ -0,0 +1,8 @@
+package com.example.androidthingsclient.models
+
+/**
+ * Created by aiachimov on 6/13/17.
+ */
+data class Humidity(private val qrCodeId: String, private val value: String, private val time: String, private val type: String) {
+    //The compiler automatically derives the following equals(), hashCode(), toString(), copy()
+}
