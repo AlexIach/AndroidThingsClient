8e766c8d513e6eadd18fc45152d76755ecc4bd2e
Alexandru Iachimov <Alexandru.Iachimov@endava.com>
2017-06-26 14:52:39 +0300

    Added progress bar for first loading, some UI modifications, added displaying others TemperatureIndocators fields

--- AndroidThingsClient/app/src/main/java/com/example/androidthingsclient/view/mainScreen/core/fragments/temperature_fragment/TemperatureFragment.java
+++ AndroidThingsClient/app/src/main/java/com/example/androidthingsclient/view/mainScreen/core/fragments/temperature_fragment/TemperatureFragment.java
@@ -1,16 +1,20 @@
 package com.example.androidthingsclient.view.mainScreen.core.fragments.temperature_fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.androidthingsclient.Injector;
 import com.example.androidthingsclient.R;
-import com.example.androidthingsclient.models.SensorsData;
 import com.example.androidthingsclient.models.TemperatureIndicators;
 import com.example.androidthingsclient.view.mainScreen.core.fragments.temperature_fragment.core.TemperaturePresenter;
 
@@ -18,6 +22,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -26,8 +31,22 @@ import butterknife.ButterKnife;
 
 public class TemperatureFragment extends Fragment implements TemperaturePresenter.TemperatureCallBack {
 
+
+    private static final int SLEEP_MILISEC = 2000;
     @Inject
     TemperaturePresenter temperaturePresenter;
+
+    @BindView(R.id.constraintLayoutTemperature)
+    ConstraintLayout constraintLayoutTemperature;
+    @BindView(R.id.textViewTemperatureValue)
+    TextView textViewTemperatureValue;
+    @BindView(R.id.textViewTemperatureType)
+    TextView textViewTemperatureType;
+    @BindView(R.id.textViewTemperatureTime)
+    TextView textViewTemperatureTime;
+
+    @BindView(R.id.lottie_animation_view_loading)
+    LottieAnimationView lottieAnimationView;
 
     public static TemperatureFragment newInstance() {
         return new TemperatureFragment();
@@ -56,16 +75,52 @@ public class TemperatureFragment extends Fragment implements TemperaturePresente
     @Override
     public void onResume() {
         super.onResume();
-        temperaturePresenter.loadTemperature();
+        new Task().execute();
     }
 
     @Override
     public void onTemperatureLoaded(List<TemperatureIndicators> temperatureList) {
         Log.d("TAG", "TemperatureFragmentList size is " + temperatureList.size());
+        TemperatureIndicators currentTemperature = temperatureList.get(temperatureList.size() - 1);
+        textViewTemperatureValue.setText("Temperature  Value is " + currentTemperature.getValue());
+        textViewTemperatureType.setText("Temperature  Type is " + currentTemperature.getType());
+        textViewTemperatureTime.setText("Last synced at " + currentTemperature.getTime());
+
     }
 
     @Override
     public void onFailedQRCodeSave(String error) {
         Log.d("TAG", "Error is " + error);
+    }
+
+    class Task extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            textViewTemperatureValue.setVisibility(View.GONE);
+            textViewTemperatureType.setVisibility(View.GONE);
+            textViewTemperatureTime.setVisibility(View.GONE);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            textViewTemperatureValue.setVisibility(View.VISIBLE);
+            textViewTemperatureType.setVisibility(View.VISIBLE);
+            textViewTemperatureTime.setVisibility(View.VISIBLE);
+            lottieAnimationView.setVisibility(View.GONE);
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try {
+                temperaturePresenter.loadTemperature();
+                Thread.sleep(SLEEP_MILISEC);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
